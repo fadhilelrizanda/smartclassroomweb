75c028d6917b14e891f3f9e33faa701a341cf102
fadhilelrizanda <fadhilelrizanda@gmail.com>
2023-08-05 00:22:06 +0700

    fix 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./app.css";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
