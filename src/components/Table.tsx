b813132cb912dd17c3d39e24de62b6192ec63090
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-03-25 13:15:28 +0700

    fix times in update

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,11 +6,18 @@ import * as FileSaver from "file-saver";
 import * as XLSX from "xlsx";
 
 function Table() {
-  const ExportToExcel = (apiData: any, fileName: string) => {
+  const ExportToExcel = (apiData: any, fileName: any) => {
     const fileType =
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
     const fileExtension = ".xlsx";
-    const ws = XLSX.utils.json_to_sheet(apiData);
+
+    // Format the timestamp using moment.js
+    const formattedData = apiData.map((data: any) => ({
+      ...data,
+      updatedAt: moment(data.updatedAt).format("YYYY-MM-DD HH:mm:ss"),
+    }));
+
+    const ws = XLSX.utils.json_to_sheet(formattedData);
     const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
     const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
     const data = new Blob([excelBuffer], { type: fileType });
